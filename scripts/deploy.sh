1be17e102c114a4a9e5196544bbf5600d6167060
Spicer Matthews <spicer@cloudmanic.com>
2016-01-07 10:16:46 -0800

    Removed unwanted code.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,14 +13,3 @@ git push origin master
 cd ../scripts
 
 ssh -p 9022 web10.cloudmanic.com 'cd /var/www/stockpeer.com && git pull origin master && php artisan migrate --force && composer update'
-
-ACCESS_TOKEN=b6df56521d174d27b37c02b5c3869b17
-ENVIRONMENT=production
-LOCAL_USERNAME=`whoami`
-REVISION=`cd ../ && git log -n 1 --pretty=format:"%H" && cd scripts`
-
-curl https://api.rollbar.com/api/1/deploy/ \
-  -F access_token=$ACCESS_TOKEN \
-  -F environment=$ENVIRONMENT \
-  -F revision=$REVISION \
-  -F local_username=$LOCAL_USERNAME
