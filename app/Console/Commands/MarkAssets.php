09c8498bf48db67127827f11d2461b21e8c6813c
Spicer Matthews <spicer@cloudmanic.com>
2017-05-24 12:32:14 -0700

    Fixed bug with mark assets

--- app/Console/Commands/MarkAssets.php
+++ app/Console/Commands/MarkAssets.php
@@ -57,8 +57,8 @@ class MarkAssets extends Command
         case 'Coinbase':
           $this->_process_coinbase($row);
         break;                
-      }
 */  
+      }
     
       // Logout
       Auth::logout();
